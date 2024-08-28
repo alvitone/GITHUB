d2f4ff0eb568e744913dd38c3a64825fe7055719
alvitone <alvitone1090@gmail.com>
2024-08-28 16:35:02 +0530

    binary tree insert code

---
+++ binaryTree.js
@@ -0,0 +1,44 @@
+class node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+class Tree {
+    constructor() {
+        this.root = null
+    }
+    insert(value) {
+        let newnode = new node(value)
+        if (this.root === null) {
+            this.root = newnode
+            return this
+        }
+        let current = this.root
+        while (true) {
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newnode
+                    return this
+                } else {
+                    current = current.left
+                }
+            } else {
+                if (current.right === null) {
+                    current.right = newnode
+                    return this
+                } else {
+                    current = current.right
+                }
+
+            }
+
+        }
+    }
+}
+let myTree = new Tree()
+myTree.insert(10)
+myTree.insert(20)
+myTree.insert(30)
+console.log(myTree.root.right.value)
